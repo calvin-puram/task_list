6e03b3d0e949020098744678f87bd71716e9276d
calvin <puram.calvin@gmail.com>
2020-04-03 11:35:11 +0100

    created balance component boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "./components/Header";
+import Balance from "./components/Balance";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6 mx-auto'>
+            <Balance />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Balance = () => {
+  return (
+    <div className='card text-center mt-5'>
+      <h4>Your Balance</h4>
+      <p>$0.00</p>
+    </div>
+  );
+};
+
+export default Balance;
