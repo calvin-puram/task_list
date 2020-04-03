c3608bc815f1a6ca343c6154014c2f3705f276a3
calvin <puram.calvin@gmail.com>
2020-04-03 11:30:44 +0100

    created header component

--- src/Footer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer = () => {
-  return <div className='footer'>All Right Reserved &copy; 2020</div>;
-};
-
-export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className='bg-dark text-white p-2 text-center'>
+      <h1>Expense Tracker</h1>
+    </div>
+  );
+};
+
+export default Header;
