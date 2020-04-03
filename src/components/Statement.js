9f588debbeec1b849da000e9b7c3eeaffff60065
calvin <puram.calvin@gmail.com>
2020-04-03 13:47:03 +0100

    completed the account statement section

--- src/components/Statement.js
+++ src/components/Statement.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Statement = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  function total() {
+    let expense = [];
+    let income = [];
+    transactions.map(trans => {
+      if (trans.amount.startsWith("-")) {
+        expense.push(trans.amount * 1);
+      } else {
+        income.push(trans.amount * 1);
+      }
+    });
+    return {
+      expense: expense.reduce((c, a) => c + a, 0),
+      income: income.reduce((c, a) => c + a, 0)
+    };
+  }
+
   return (
     <div className='card mt-3 container'>
       <div className='d-flex justify-content-between align-items-center'>
         <div className='text-center pt-2'>
           Income
-          <p className='text-success'>$0.00</p>
+          <p className='text-success'>${total().income}</p>
         </div>
 
         <div className='text-center pt-2'>
           Expense
-          <p className='text-danger'>$0.00</p>
+          <p className='text-danger'>-${Math.abs(total().expense)}</p>
         </div>
       </div>
     </div>
