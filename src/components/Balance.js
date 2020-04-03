ecf8b41c5d233f76f28b33b4e4ba6d7a6e944280
calvin <puram.calvin@gmail.com>
2020-04-03 13:30:12 +0100

    added state to balance component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const total = () => {
+    const amount = transactions.map(trans => trans.amount * 1);
+    return amount.reduce((c, a) => c + a, 0);
+  };
+
   return (
     <div className='card text-center mt-5'>
       <div className='pt-2'>
         <h4>Your Balance</h4>
-        <p>$0.00</p>
+        <p>${total()}</p>
       </div>
     </div>
   );
